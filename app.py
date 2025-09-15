7332a67bc511b490c163d0c73f7335c4d4d93e18
ProblematicGoose <one.dollar.value@gmail.com>
2025-09-15 08:08:54 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from datetime import datetime, timedelta, date
 # -----------------------------
 # Config / Endpoints
 # -----------------------------
-BASE_URL = "https://fcd153388690.ngrok-free.app"
+BASE_URL = "https://85f53f27b810.ngrok-free.app"
 SCORECARD_URL = f"{BASE_URL}/api/scorecard"
 PHOTOS_URL = f"{BASE_URL}/api/subject-photos"
 TIMESERIES_URL = f"{BASE_URL}/api/timeseries"
